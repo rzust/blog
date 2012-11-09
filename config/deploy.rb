68a6bf102098af66ed0c3b1e80ac0e19287fdeed
Renato Zust <renatozust@gmail.com>
2012-11-09 10:38:25 -0500

    first commit

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+require "bundler/capistrano"
+
+load "config/recipes/base"
+load "config/recipes/nginx"
+load "config/recipes/unicorn"
+load "config/recipes/postgresql"
+load "config/recipes/nodejs"
+load "config/recipes/rbenv"
+load "config/recipes/check"
+
+server "172.16.194.128", :web, :app, :db, primary: true
+
+set :user, "eightynine"
+set :application, "blog"
+set :deploy_to, "/var/www/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:rzust/#{application}.git"
+set :branch, "master"
+
+set :maintenance_template_path, File.expand_path("../recipes/templates/maintenance.html.erb", __FILE__)
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
