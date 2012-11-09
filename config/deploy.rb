2fd97a9a3b2387dd0859024b77fbacd4b8b608ec
Renato Zust <renatozust@gmail.com>
2012-11-09 15:06:38 -0500

    sadf

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,24 +9,38 @@ load "config/recipes/nodejs"
 load "config/recipes/rbenv"
 load "config/recipes/check"
 
-server "172.16.194.128", :web, :app, :db, primary: true
-
-set :user, "eightynine"
 set :application, "blog"
-set :deploy_to, "/var/www/apps/#{application}"
-set :deploy_via, :remote_cache
 set :use_sudo, false
 set :rvm_type, :system
 set :rvm_ruby_string, "ruby-1.9.3-p0@blog"
 
 set :scm, "git"
 set :repository, "git@github.com:rzust/#{application}.git"
-set :branch, "master"
 
 set :maintenance_template_path, File.expand_path("../recipes/templates/maintenance.html.erb", __FILE__)
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
+
+task :production do
+  server "172.16.194.128", :web, :app, :db, primary: true
+  set :user, "eightynine"
+  set :deploy_to, "/var/www/apps/#{application}/"
+  set :deploy_via, :remote_cache
+  set :branch, "production"
+  after('deploy:symlink', 'cache:clear')
+end
+
+task :staging do
+  server "192.168.1.42", :web, :app, :db, primary: true
+  set :user, "senddinero"
+  set :deploy_to, "/var/www/apps/#{application}/"
+  set :deploy_via, :copy
+  set :branch, "staging" 
+  after('deploy:symlink', 'cruise_control:build')
+end
+
+
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
