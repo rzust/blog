994c9f06ea3e5e05fd32f33f4f23dc65bef50a35
Renato Zust <renatozust@gmail.com>
2012-11-11 14:54:37 -0500

    SD

--- config/recipes/postgresql.rb
+++ config/recipes/postgresql.rb
@@ -21,7 +21,7 @@ namespace :postgresql do
 
   desc "Create a database for this application."
   task :create_database, roles: :db, only: {primary: true} do
-    run %Q{mysql -u #{user} -p -e "CREATE DATABASE #{mysql_database};"} 
+    run %Q{mysql -u #{user} -p #{mysql_password} -e "CREATE DATABASE #{mysql_database};"} 
   end
 
   desc "Generate the database.yml configuration file."
