894966fc86953a9f01f7b4d1d8113df3af79dac3
Renato Zust <renatozust@gmail.com>
2012-11-11 14:52:32 -0500

    asdf

--- config/recipes/postgresql.rb
+++ config/recipes/postgresql.rb
@@ -1,7 +1,7 @@
-set_default(:postgresql_host, "localhost")
-set_default(:postgresql_user) { "root" }
-set_default(:postgresql_password) { Capistrano::CLI.password_prompt "MySQL Password: " }
-set_default(:postgresql_database) { "#{application}_production" }
+set_default(:mysql_host, "localhost")
+set_default(:mysql_user) { "deploy" }
+set_default(:mysql_password) { Capistrano::CLI.password_prompt "MySQL Password: " }
+set_default(:mysql_database) { "#{application}_production" }
 
 namespace :postgresql do
   desc "Install the latest stable release of PostgreSQL."
@@ -13,11 +13,16 @@ namespace :postgresql do
   #after "deploy:install", "postgresql:install"
 
   desc "Create a database for this application."
-  task :create_database, roles: :db, only: {primary: true} do
-    run %Q{#{sudo} -u postgres psql -c "create user #{postgresql_user} with password '#{postgresql_password}';"}
-    run %Q{#{sudo} -u postgres psql -c "create database #{postgresql_database} owner #{postgresql_user};"}
-  end
+  # task :create_database, roles: :db, only: {primary: true} do
+  #   run %Q{#{sudo} -u postgres psql -c "create user #{postgresql_user} with password '#{postgresql_password}';"}
+  #   run %Q{#{sudo} -u postgres psql -c "create database #{postgresql_database} owner #{postgresql_user};"}
+  # end
   #after "deploy:setup", "postgresql:create_database"
+
+  desc "Create a database for this application."
+  task :create_database, roles: :db, only: {primary: true} do
+    run %Q{mysql -u #{user} -p -e "CREATE DATABASE #{mysql_database};"} 
+  end
 
   desc "Generate the database.yml configuration file."
   task :setup, roles: :app do
