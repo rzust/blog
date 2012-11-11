7c3e22c1bb05f7bd460308d3e7e1cdc62146f1ac
Renato Zust <renatozust@gmail.com>
2012-11-11 15:03:30 -0500

    asdf

--- config/recipes/postgresql.rb
+++ config/recipes/postgresql.rb
@@ -1,5 +1,5 @@
 set_default(:mysql_host, "localhost")
-set_default(:mysql_user) { "deploy" }
+set(:mysql_user, Capistrano::CLI.ui.ask("User name: ") )
 set_default(:mysql_password) { Capistrano::CLI.password_prompt "MySQL Password: " }
 set_default(:mysql_database) { "#{application}_production" }
 
@@ -21,7 +21,12 @@ namespace :postgresql do
 
   desc "Create a database for this application."
   task :create_database, roles: :db, only: {primary: true} do
-    run %Q{mysql -u #{user} -p #{mysql_password} -e "CREATE DATABASE #{mysql_database};"} 
+    cmd = "CREATE DATABASE IF NOT EXISTS #{mysql_database}"
+    run "mysql -u #{mysql_user} -p -e '#{cmd}'" do |channel, stream, data|
+      if data =~ /^Enter password:/
+         channel.send_data "#{mysql_password}\n"
+       end
+    end       
   end
 
   desc "Generate the database.yml configuration file."
